6f29f798a2f4f8a0144d6798612094057c6c5170
Dallas S. <andrewdallassherman@gmail.com>
2025-01-05 09:05:24 +0900

    edits... figuring it out

--- backend/usecase/users.go
+++ backend/usecase/users.go
@@ -1,46 +1,9 @@
 package usecase
 
 import (
-	"log"
-	"main/model"
 	"main/repository"
-
-	"github.com/google/uuid"
 )
 
 type UserService struct {
 	repo repository.UsersRepo
-}
-
-func GenerateUserID() string {
-	id, err := uuid.NewUUID()
-	if err != nil {
-		log.Fatal("Failed to generate a unique ID", err)
-	}
-	return id.String()
-}
-
-// CreateUser is a function to create a new user
-func CreateUser(input model.User (models.Users, error) {
-	// Generate a new UserID
-	userID, err := GenerateUserID()
-	if err != nil {
-		return model.Users{}, err // Handle error if UserID generation fails
-	}
-
-	// Populate the Users struct with generated UserID
-	newUser := model.Users{
-		UserID:   userID, // Assign the generated ID here
-		Name:     input.Name,
-		Email:    input.Email,
-		Password: input.Password, // Assuming it's hashed
-	}
-
-	// Perform any necessary database operations here
-	err = saveUserToDB(newUser) // Replace with your actual DB call
-	if err != nil {
-		return models.Users{}, err
-	}
-
-	return newUser, nil
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,3 @@
+import { useEffect, useState } from "react";
+import { getPopularMovies, searchMovies } from "../services/api.js";
+import "../css/Home.css";
