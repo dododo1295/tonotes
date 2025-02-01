051169ea0d59bd985cf3bddb47c56ab2c8010787
Dallas Sherman <dododo@Dallass-MacBook-Air.local>
2025-02-01 16:03:56 +0900

    model updates

--- backend/model/todos.go
+++ backend/model/todos.go
@@ -2,6 +2,14 @@ package model
 
 import "time"
 
+type Priority string
+
+const (
+	PriorityLow    Priority = "LOW"
+	PriorityMedium Priority = "MEDIUM"
+	PriorityHigh   Priority = "HIGH"
+)
+
 type Todos struct {
 	TodoID      string    `bson:"_id,omitempty" json:"id"`
 	UserID      string    `bson:"user_id" json:"user_id"`
@@ -11,4 +19,7 @@ type Todos struct {
 	CreatedAt   time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt   time.Time `bson:"updated_at" json:"updated_at"`
 	Tags        []string  `bson:"tags,omitempty" json:"tags,omitempty"`
+	Priority    Priority  `bson:"priority" json:"priority"`
+	DueDate     time.Time `bson:"due_date" json:"due_date"`
+	ReminderAt  time.Time `bson:"reminder_at" json:"reminder_at"`
 }
