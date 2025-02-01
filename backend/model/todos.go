e3a7cdc5846bc31ee14b43b147ee07f783e971f3
Dallas Sherman <dododo@Dallass-MacBook-Air.local>
2025-02-01 10:50:14 +0900

    naming scheme update

--- backend/model/todos.go
+++ backend/model/todos.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Todos struct {
-	ID              string    `bson:"_id,omitempty" json:"id"`
-	UserID          string    `bson:"user_id" json:"user_id"`
-	TodoName        string    `bson:"todo_name" json:"todo_name" binding:"required"`
-	TodoDescription string    `bson:"todo_description" json:"todo_description"`
-	Complete        bool      `bson:"complete" json:"complete"`
-	CreatedAt       time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt       time.Time `bson:"updated_at" json:"updated_at"`
-	Tags            []string  `bson:"tags,omitempty" json:"tags,omitempty"`
+	TodoID      string    `bson:"_id,omitempty" json:"id"`
+	UserID      string    `bson:"user_id" json:"user_id"`
+	TodoName    string    `bson:"todo_name" json:"todo_name" binding:"required"`
+	Description string    `bson:"todo_description" json:"todo_description"`
+	Complete    bool      `bson:"complete" json:"complete"`
+	CreatedAt   time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `bson:"updated_at" json:"updated_at"`
+	Tags        []string  `bson:"tags,omitempty" json:"tags,omitempty"`
 }
