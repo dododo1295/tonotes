7d0bbe0cbb9ec45e364a5bcfe446e51790d718e5
Dallas. S <andrewdallassherman@gmail.com>
2025-03-02 17:57:18 +0900

    migrating backend out

--- backend/utils/base_url.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "github.com/gin-gonic/gin"
-
-// Helper function for getting baseURL of project
-func GetBaseURL(c *gin.Context) string {
-	return c.Request.URL.Scheme + "://" + c.Request.Host + "/api/v1"
-}
